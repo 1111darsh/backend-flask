ea1fc503db0f97fc6be6813877f7fcacbe196aaa
1111darsh <1111darsh@gmail.com>
2023-12-07 15:21:23 +0530

    Create the simple the python applictioan in the flask to Understand the api

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # backend-flask
 This is a simple flask Application
+
+
+**GET** /tasks: Retrieves all tasks.
+**GET** /tasks/<task_id>: Retrieves a specific task by its ID.
+**POST** /tasks: Creates a new task.
+**PUT** /tasks/<task_id>: Updates an existing task.
+**PATCH** /tasks/<task_id>: Partially updates an existing task.
+**DELETE** /tasks/<task_id>: Deletes an existing task.
+
+
+
+
+Postman Collections [link](./backend-flask.postman_collection.json)
---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+tasks = [
+    {"id": 1, "title": "Task 1", "description": "This is task 1", "done": False},
+    {"id": 2, "title": "Task 2", "description": "This is task 2", "done": False},
+]
+
+
+# GET - Retrieve all tasks
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+# GET - Retrieve a specific task
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        return jsonify({'error': 'Task not found'}), 404
+    return jsonify({'task': task[0]})
+
+
+# POST - Create a new task
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    new_task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ''),
+        'done': False
+    }
+    tasks.append(new_task)
+    return jsonify({'task': new_task}), 201
+
+
+# PUT - Update an existing task
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        return jsonify({'error': 'Task not found'}), 404
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+
+# PATCH - Partially update an existing task
+@app.route('/tasks/<int:task_id>', methods=['PATCH'])
+def partial_update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        return jsonify({'error': 'Task not found'}), 404
+    if 'title' in request.json:
+        task[0]['title'] = request.json['title']
+    if 'description' in request.json:
+        task[0]['description'] = request.json['description']
+    if 'done' in request.json:
+        task[0]['done'] = request.json['done']
+    return jsonify({'task': task[0]})
+
+
+# DELETE - Delete an existing task
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        return jsonify({'error': 'Task not found'}), 404
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
